4229d3ddf0c9213913aae65d2b821d9e3e16a7a6
iago <iagodeaquino10@gmail.com>
2025-06-18 11:05:39 -0300

    Home page created

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IonCol, IonGrid, IonRow, IonContent } from '@ionic/angular/standalone';
+import cardContainerModule from '../poke-card-component/card-container.module';
+import { CommonModule } from '@angular/common';
+import ApiHandler from '../injectables/data-handler.injectable';
+@Component({
+  selector: 'home-page',
+  templateUrl: './home.page.html',
+  imports: [
+    IonCol,
+    IonGrid,
+    IonRow,
+    cardContainerModule,
+    CommonModule,
+    IonContent,
+  ],
+  styleUrls: ['home.page.scss'],
+  providers: [ApiHandler],
+})
+export default class HomePage {
+  constructor(private api_handler: ApiHandler) {}
+
+  public pokemon_list = [];
+  public pokemon_data: Record<string, any> = {};
+
+  async ngOnInit() {
+    this.pokemon_list = (await this.api_handler.getPokemonList())['results'];
+    this.pokemon_data = this.api_handler.getDataStored();
+    console.log(this.pokemon_list);
+    console.log(this.pokemon_data);
+  }
+}
