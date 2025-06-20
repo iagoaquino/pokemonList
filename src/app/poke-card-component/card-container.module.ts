4691d819deb737739cda1a0ee9b3cea6a49c0db5
iago <iagodeaquino10@gmail.com>
2025-06-19 22:53:32 -0300

    New set favorite button in cards to include the pokemon in favorites

--- src/app/poke-card-component/card-container.module.ts
+++ src/app/poke-card-component/card-container.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import CardContainerComponent from './card-container.component';
 import { PokemonDescriptionModalModule } from '../pokemon-description-component/pokemon-description-modal.module';
-
+import { CommonModule } from '@angular/common';
 @NgModule({
-  imports: [IonicModule, PokemonDescriptionModalModule],
+  imports: [IonicModule, PokemonDescriptionModalModule, CommonModule],
   exports: [CardContainerComponent],
   declarations: [CardContainerComponent],
 })
