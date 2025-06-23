3c4b424be99d9e5071a51d72daeb62f5fe948289
iago <iagodeaquino10@gmail.com>
2025-06-23 20:57:20 -0300

    Pokemon list and home page changed to use the new data storager and type filter

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,30 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  IonCol,
-  IonGrid,
-  IonRow,
-  IonTitle,
-  IonHeader,
-} from '@ionic/angular/standalone';
-import PokemonListModule from '../pokemon-list-component/pokemon-list.module';
-import cardContainerModule from '../poke-card-component/card-container.module';
+import { IonicModule } from '@ionic/angular';
+
 import ApiHandler from '../injectables/data-handler.injectable';
+import PokemonListComponent from '../pokemon-list-component/pokemon-list.component';
+import SearchComponent from '../search-component/search.component';
 
 @Component({
   selector: 'home-page',
   templateUrl: './home.page.html',
-  imports: [
-    cardContainerModule,
-    CommonModule,
-    PokemonListModule,
-    IonCol,
-    IonGrid,
-    IonRow,
-    IonHeader,
-    IonTitle,
-  ],
+  imports: [CommonModule, PokemonListComponent, SearchComponent, IonicModule],
   styleUrls: ['home.page.scss', '../globals.style.scss'],
   providers: [ApiHandler],
+  standalone: true,
 })
-export default class HomePage {}
+export default class HomePage {
+  private types_filter: Array<string> = [
+    'fire',
+    'water',
+    'grass',
+    'bug',
+    'poison',
+    'flying',
+    'steel',
+    'normal',
+    'fairy',
+    'ground',
+    'fighting',
+    'psychic',
+    'rock',
+    'electric',
+    'ghost',
+    'dragon',
+    'ice',
+    'dark',
+  ];
+  @Output() handle_update = new EventEmitter<boolean>();
+
+  handleFilterChange(event: Event | Array<string>) {
+    this.types_filter = event as Array<string>;
+  }
+
+  get get_types_filter(): Array<string> {
+    return this.types_filter;
+  }
+}
--- src/app/pokemon-list-component/pokemon-list.component.ts
+++ src/app/pokemon-list-component/pokemon-list.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChange } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+
 import FavoriteEventInterface from '../interfaces/favorite-handler.interface';
-import ApiHandler from '../injectables/data-handler.injectable';
+import DataHandler from '../injectables/data-handler.injectable';
+import CardContainerComponent from '../poke-card-component/card-container.component';
 
 @Component({
   selector: 'pokemon-list-component',
@@ -10,41 +14,79 @@ import ApiHandler from '../injectables/data-handler.injectable';
     '../types-color.style.scss',
     '../globals.style.scss',
   ],
-  standalone: false,
+  imports: [IonicModule, CardContainerComponent, CommonModule],
 })
 export default class PokemonListComponent {
-  constructor(private api_handler: ApiHandler) {}
+  constructor(private data_handler: DataHandler) {}
 
-  public next_url!: string | null;
-  public prev_url!: string | null;
-  public pokemon_list = [] as Array<Record<string, any>>;
-  public pokemon_data: Record<string, any> = {};
-  public favorite_list: Array<string> = [];
-  public favorite_pokemon_data: Record<string, any> = {};
-  public show_only_favorite: boolean = false;
+  @Input() filter_list: Array<string> = [];
 
-  async ngOnInit() {
-    this.getPokemonList(`https://pokeapi.co/api/v2/pokemon?limit=12&offset=0`);
+  private initial_position: number = 0;
+  private final_position: number = 9;
+  private incrementer: number = 9;
+  private pokemon_list = [] as Array<string>;
+  private pokemon_data: Record<string, any> = {};
+  private favorite_list: Array<string> = [];
+  private favorite_pokemon_data: Record<string, any> = {};
+  private show_only_favorite: boolean = false;
+
+  get get_show_only_favorite() {
+    return this.show_only_favorite;
   }
 
-  async getPokemonList(url: string) {
-    const data_requested = await this.api_handler.getPokemonData(url);
-    this.pokemon_list = data_requested['results'];
-    this.next_url = data_requested['next'];
-    this.prev_url = data_requested['previous'];
-    this.pokemon_data = this.api_handler.getDataStored();
-    this.favorite_list = this.api_handler.getFavoriteList();
+  get get_favorite_list() {
+    return this.favorite_list;
+  }
+
+  get get_pokemon_data() {
+    return this.pokemon_data;
+  }
+
+  get get_pokemon_list() {
+    return this.pokemon_list;
+  }
+
+  get get_initial_position() {
+    return this.initial_position;
+  }
+
+  get get_final_position() {
+    return this.final_position;
+  }
+
+  get get_favorite_pokemon_data() {
+    return this.favorite_pokemon_data;
+  }
+
+  async ngOnInit() {
+    await this.data_handler.updateDateStored(this.filter_list);
+    this.getPokemonList();
+  }
+
+  async ngOnChanges(change: SimpleChange) {
+    await this.data_handler.updateDateStored(this.filter_list);
+    this.getPokemonList();
+  }
+
+  async getPokemonList() {
+    const data_requested = this.data_handler.getPokemonsData(
+      this.initial_position,
+      this.final_position
+    );
+
+    this.pokemon_list = data_requested['name_list'];
+    this.pokemon_data = data_requested['pokemons_data'];
   }
 
   async favoritehandler(event: Event | FavoriteEventInterface) {
     const event_sended = event as FavoriteEventInterface;
     if (event_sended.type === 'insertion') {
-      this.api_handler.addNewFavorite(event_sended.name);
+      this.data_handler.addNewFavorite(event_sended.name);
       this.favorite_pokemon_data[event_sended.name] = event_sended.pokemon_data;
     } else {
-      this.api_handler.deleteFavorite(event_sended.name);
+      this.data_handler.deleteFavorite(event_sended.name);
     }
-    this.favorite_list = this.api_handler.getFavoriteList();
+    this.favorite_list = this.data_handler.getFavoriteList();
   }
 
   SwapShowOnlyFavorite() {
@@ -52,10 +94,14 @@ export default class PokemonListComponent {
   }
 
   async navigationHandler(type: 'prev' | 'next') {
-    if (type === 'next') {
-      this.getPokemonList(this.next_url as string);
-    } else {
-      this.getPokemonList(this.prev_url as string);
-    }
+    this.initial_position =
+      type === 'next'
+        ? this.initial_position + this.incrementer
+        : this.initial_position - this.incrementer;
+    this.final_position =
+      type === 'next'
+        ? this.final_position + this.incrementer
+        : this.final_position - this.incrementer;
+    this.getPokemonList();
   }
 }
